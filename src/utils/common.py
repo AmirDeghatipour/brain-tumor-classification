fc2aad8631b52a46e6469b3d5f26d51c03f421a1
Amir Deghatipour <a.deghatipour@gmail.com>
2025-05-04 20:43:42 +0330

    create utils

---
+++ src/utils/common.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from box import ConfigBox
+from box.exceptions import BoxValueError
+import yaml
+from src.logging import logger
+from ensure import ensure_annotations
+
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """Reads YAML file and returns its content as ConfigBox."""
+    try:
+        with path_to_yaml.open("r") as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"YAML file loaded successfully: {path_to_yaml}")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("YAML file is empty")
+    except Exception as e:
+        raise e
+
+
+@ensure_annotations
+def create_directories(paths: list[Path], verbose: bool = True):
+    """Creates a list of directories."""
+    for path in paths:
+        path.mkdir(parents=True, exist_ok=True)
+        if verbose:
+            logger.info(f"Created directory at: {path}")
